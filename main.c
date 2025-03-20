b321195eaf29d936bd080ddcea5a4f65b8286e70
morthaurios <joerikmontes@hotmail.com>
2025-03-20 00:43:35 -0600

    Integrate sensor

--- main.c
+++ main.c
@@ -1,17 +1,44 @@
 #include "I2c.h"
 #include "Mem.h"
+#include "Sensor.h"
+static union
+{
+    uint8_t au8Bytes[256];
+    uint32_t au8DWord[64];
+} uMeasurements;
 
-const static uint8_t au8TxBuffer[256];
-static uint8_t au8RxBuffer[256];
 
 void main(void)
 {
-    I2C_Init();
-    Mem_Init();
+    uint8_t u8MeasurementIndex = 0U;
+
+    if(E_OK != I2C_Init())
+    {
+        /* Trigger warning */
+    }
+
+    if(E_OK != Mem_Init())
+    {
+        /* Trigger warning */
+    }
+
+    if(E_OK != Sensor_Init())
+    {
+        /* Trigger warning */
+    }
 
     for(;;)
     {
-        I2C_Transmit(0x20U, au8TxBuffer, 10U);
-        I2C_Transmit(0x20U, au8RxBuffer, 10U);
+        if(E_OK == Sensor_Read(&uMeasurements.au8DWord[u8MeasurementIndex]))
+        {
+            u8MeasurementIndex++;
+        }
+
+        if(u8MeasurementIndex >= 64U)
+        {
+            Mem_Write(0x800U, uMeasurements.au8Bytes, (uint16_t) sizeof(uMeasurements));
+
+            u8MeasurementIndex = 0U;
+        }
     }
 }
